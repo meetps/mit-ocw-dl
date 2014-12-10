2a4df6ae27e0c1ec9711b0fa703d360197980276
Meet Shah <meetshah1995@iitb.ac.in>
2014-12-10 12:07:26 +0530

    Added link as system argument

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
-from HTMLParser import HTMLParser
+import sys
 import urllib
 import os
+from HTMLParser import HTMLParser
 
 base_url = 'http://ocw.mit.edu'
 lec_url_list = []
@@ -37,8 +38,12 @@ class vidHTMLParser(HTMLParser):
 					video_url_list.append(value)
 				break
 
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
 lecLinkParser = lecHTMLParser()
-f = urllib.urlopen("sample.html")
+f = urllib.urlopen(str(sys.argv[1]))
 lec_html = f.read()
 lecLinkParser.feed(lec_html)
 lecLinkParser.close()
@@ -56,10 +61,8 @@ for duplicate in video_url_list:
 		video_url_list.remove(duplicate)
 	i = i + 1
 
-j=0
+'''j=0
 for vid_url in video_url_list:
 	print "Downloading Lecture " , j+1 , vid_name_list[j]
 	urllib.urlretrieve(vid_url,vid_name_list[j]+".mp4")
-	j = j + 1
-
-
+	j = j + 1'''
