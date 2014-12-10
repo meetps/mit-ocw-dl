49907ab5c67760ff26ea62fadc48b24a6d8f13dd
Meet Shah <meetshah1995@iitb.ac.in>
2014-12-10 12:05:02 +0530

    VideoNames are lecture Names

--- download.py
+++ download.py
@@ -1,10 +1,17 @@
 from HTMLParser import HTMLParser
 import urllib
-import os	
+import os
 
 base_url = 'http://ocw.mit.edu'
 lec_url_list = []
 video_url_list = []
+vid_name_list = []
+
+def findName(url):
+	cut_position = url.rfind("/")
+	name = url[cut_position + 1:]
+	return name
+
 
 class lecHTMLParser(HTMLParser):
 	def handle_starttag(self, tag, attrs):
@@ -15,7 +22,10 @@ class lecHTMLParser(HTMLParser):
 					flag =1
 				if(key == 'href' and flag == 1):
 					print "link : ",value
+					lec_name = findName(value)
+					print "Lecture Name :",lec_name
 					lec_url_list.append(value)
+					vid_name_list.append(lec_name)
 					flag = 0
 
 class vidHTMLParser(HTMLParser):
@@ -33,8 +43,23 @@ lec_html = f.read()
 lecLinkParser.feed(lec_html)
 lecLinkParser.close()
 
-videoLinkParser = vidHTMLParser()
-g = urllib.urlopen("video.html")
-vid_html = g.read()
-videoLinkParser.feed(vid_html)
-videoLinkParser.close()
+for lec_url in lec_url_list:
+	response = urllib.urlopen(base_url + lec_url)
+	html = response.read()
+	videoParser = vidHTMLParser()
+	videoParser.feed(html)
+	videoParser.close()
+
+i=0
+for duplicate in video_url_list:
+	if(i%2 == 1):
+		video_url_list.remove(duplicate)
+	i = i + 1
+
+j=0
+for vid_url in video_url_list:
+	print "Downloading Lecture " , j+1 , vid_name_list[j]
+	urllib.urlretrieve(vid_url,vid_name_list[j]+".mp4")
+	j = j + 1
+
+
--- sample2.py
+++ sample2.py
@@ -1,6 +1,7 @@
-from HTMLParser import HTMLParser
+import sys
 import urllib
 import os
+from HTMLParser import HTMLParser
 
 base_url = 'http://ocw.mit.edu'
 lec_url_list = []
@@ -37,17 +38,15 @@ class vidHTMLParser(HTMLParser):
 					video_url_list.append(value)
 				break
 
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
 lecLinkParser = lecHTMLParser()
-f = urllib.urlopen("sample.html")
+f = urllib.urlopen(str(sys.argv[1]))
 lec_html = f.read()
 lecLinkParser.feed(lec_html)
 lecLinkParser.close()
-
-'''videoLinkParser = vidHTMLParser()
-g = urllib.urlopen("video.html")
-vid_html = g.read()
-videoLinkParser.feed(vid_html)
-videoLinkParser.close()'''
 
 for lec_url in lec_url_list:
 	response = urllib.urlopen(base_url + lec_url)
@@ -62,10 +61,8 @@ for duplicate in video_url_list:
 		video_url_list.remove(duplicate)
 	i = i + 1
 
-j=0
+'''j=0
 for vid_url in video_url_list:
-	print "Downloading Lecture " , j , vid_name_list[j]
+	print "Downloading Lecture " , j+1 , vid_name_list[j]
 	urllib.urlretrieve(vid_url,vid_name_list[j]+".mp4")
-	j = j + 1
-
-
+	j = j + 1'''
